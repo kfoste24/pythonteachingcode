d49052042d0d8b426d33ac7f718b78429f790ec4
kfost <kfoste24@students.kennesaw.edu>
2019-10-21 11:03:05 -0400

    Update script1.py

--- 6 Web Page with Flask/basic page/script1.py
+++ 6 Web Page with Flask/basic page/script1.py
@@ -16,5 +16,9 @@ def home():
 def about():
     return render_template("about.html")
 
+@app.route('/kyle/')
+def kyle():
+    return render_template("kyle.html")
+
 if __name__=="__main__":
     app.run(debug=True)
